084c89c03ae317c1be6afc0948e967bc1d172b1e
Mike Cho <elegantspanishguitar@gmail.com>
2023-02-10 11:19:47 -0700

    first commit

---
+++ wp-crystall-ball.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Plugin Name: wp-crystal-ball
+* Plugin URI: https://mikecho.dev/
+* Description: Generate answers to your questions, using OpenAI
+* Version: 1.0
+* Author: Michael W. Cho
+* Author URI: https://mikecho.dev/
+**/
+
+echo 'hello, friend';
